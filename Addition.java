91a6429afcf6edfe0b6d5b52b9c182e49c4fb6c7
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-17 15:07:04 +0530

    Added Selection1

---
+++ Addition.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Addition 
+{
+    public static void main(String A[])
+    {
+        int No1 = 0, No2 = 0 , Ans = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter first no :");
+        No1 = sobj.nextInt();
+
+        System.out.println("Enter second no :");
+        No2 = sobj.nextInt();
+
+        Ans = No1 + No2;
+
+        System.out.println("Addition is : " + Ans);
+        
+    }
+}
