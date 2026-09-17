10699cff2f063425a885d386f46389b12eabdd50
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-17 15:10:11 +0530

    Added Sequence1

---
+++ Sequence1.java
@@ -0,0 +1,12 @@
+//Q) 
+
+class Sequence1
+{
+    public static void main (String A[])
+    {
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+    }
+}
