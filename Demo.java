fc3087edc869713ecba34dbfa245cdac7026bc81
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-17 15:04:00 +0530

    First code

---
+++ Demo.java
@@ -0,0 +1,14 @@
+ class Demo
+{
+    public static void main(String A[])
+    {
+        System.out.println("jay Ganesh...");
+    }
+    
+}
+
+//java Demo.java - for direct run  -- here .class create and delete automatically
+
+//javac Demo.java - for bytecode 
+//java Demo - run up cmd 
+//jve is not interested
