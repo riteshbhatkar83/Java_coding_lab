690ee32c347901faa4618c283577ff5f3e73e03a
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-18 11:18:47 +0530

    Added LB.java

---
+++ LB.java
@@ -0,0 +1,11 @@
+package Marvellous;
+
+public class LB
+{
+    public void LB_fun()
+    {
+        System.out.println("Inside fun of LB");
+    }
+}
+
+//he kay karte tar already exitsing Marvellous folder(j PPA banava chay vdes banvla) madhe LB.class file banavte
