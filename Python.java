42e6a4289b2356786584ec60ecedb9e39df7f7a3
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-18 11:19:25 +0530

    Added Python.java

---
+++ Python.java
@@ -0,0 +1,24 @@
+package Marvellous.Infosystem;
+
+public class Python
+{
+    public void Python_fun()
+    {
+        System.out.println("Inside fun of Python");
+    }
+}
+
+//marvellous folder madhe yek Infosystem folder banavun tayt python.class file banavte
+
+
+
+
+
+
+
+
+
+
+
+
+//star cha 
