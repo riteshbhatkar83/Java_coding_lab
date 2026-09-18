90c27e34f66a08c8e7753e0e3a1f2bef89adac73
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-18 11:18:39 +0530

    Added Iteration3.java

---
+++ Iteration3.java
@@ -0,0 +1,16 @@
+//Q) print jay ganesh 10 times using while loop
+
+class Iteration3
+{
+    public static void main (String A[])
+    {   
+        int i = 0;
+        i = 1;         // 1
+                // 2
+        while (i <= 4 )  
+        {
+            System.out.println("jay ganesh");  // 4 
+            i++;  // 3
+        }
+    }
+}
