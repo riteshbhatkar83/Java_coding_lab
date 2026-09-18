b3c5f6b5dccddb1e583e4bfcaf8fb926dadb648a
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-18 11:18:31 +0530

    Added Iteration2.java

---
+++ Iteration2.java
@@ -0,0 +1,15 @@
+//Q) print jay ganesh 10 times using for loop
+
+class Iteration2
+{
+    public static void main (String A[])
+    {   
+        int i = 0;
+        //     1      2     3
+        for( i = 1; i <= 10; i++ )
+        {
+             System.out.println("Jay Ganesh");    // 4
+        }
+    }
+}
+
