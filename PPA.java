a83eb00e3768175a382cfd91572e909c3bf8d6ea
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-18 11:19:07 +0530

    Added PPA.java

---
+++ PPA.java
@@ -0,0 +1,13 @@
+package Marvellous;
+
+public class PPA
+{
+    public void PPA_fun()
+    {
+        System.out.println("Inside fun of PPA");
+    }
+}
+
+
+//javac -d . PPA.java
+//he kay kar t Marvellous folder create karun tayt PPA class banavta
