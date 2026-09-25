d51114cf1c9c17737e086c629622da2ce72fa8f8
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-25 17:07:50 +0530

    Added Rit_JavaUseStaticPrimitive.java

---
+++ Rit_JavaUseStaticPrimitive.java
@@ -0,0 +1,20 @@
+public class Rit_JavaUseStaticPrimitive {
+   
+     public static void main(String[] args) {
+        
+        int age = 21;     // primitive variable
+        double marks = 85.5;
+
+        System.out.println("Age = " + age);
+        System.out.println("Marks = " + marks);
+    }
+}
+
+
+// 🔸 Output
+// Age = 21
+// Marks = 85.5
+
+// 👉 ✔ age and marks are primitive variables
+// 👉 ✔ Stored in stack memory
+// 👉 ✔ Fixed size → static allocation
